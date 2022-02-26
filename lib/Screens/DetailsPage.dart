8bd4d794cbd32494b751a0be707044d1737bfa79
Pranab1109 <pranabsaha1130@gmail.com>
2022-02-26 16:08:05 +0530

    ui changes

--- lib/Screens/DetailsPage.dart
+++ lib/Screens/DetailsPage.dart
@@ -13,96 +13,109 @@ class DetailsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.indigo[300],
-      body: Center(
-        child: Center(
-          child: GlassMorphism(
-            end: 0.7,
-            start: 0.3,
-            child: Container(
-              width: MediaQuery.of(context).size.width * 0.88,
-              height: MediaQuery.of(context).size.height * 0.5,
-              padding: const EdgeInsets.all(8.0),
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-              ),
-              child: Column(
-                children: [
-                  Hero(
-                    tag: index,
-                    child: SizedBox(
-                      width: 70.0,
-                      height: 70.0,
-                      child: crypto.image != null || crypto.image != ""
-                          ? CachedNetworkImage(
-                              colorBlendMode: BlendMode.softLight,
-                              imageUrl: crypto.image ?? "",
-                              imageBuilder: (context, imageProvider) =>
-                                  Container(
-                                decoration: BoxDecoration(
-                                  image: DecorationImage(
-                                    image: imageProvider,
-                                    fit: BoxFit.cover,
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: NetworkImage(
+            "https://images.unsplash.com/photo-1642104704074-907c0698cbd9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80",
+          ),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Center(
+          child: Center(
+            child: GlassMorphism(
+              end: 0.7,
+              start: 0.3,
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.88,
+                height: MediaQuery.of(context).size.height * 0.5,
+                padding: const EdgeInsets.all(8.0),
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+                child: Column(
+                  children: [
+                    Hero(
+                      tag: index,
+                      child: SizedBox(
+                        width: 70.0,
+                        height: 70.0,
+                        child: crypto.image != null || crypto.image != ""
+                            ? CachedNetworkImage(
+                                colorBlendMode: BlendMode.softLight,
+                                imageUrl: crypto.image ?? "",
+                                imageBuilder: (context, imageProvider) =>
+                                    Container(
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                      image: imageProvider,
+                                      fit: BoxFit.cover,
+                                    ),
                                   ),
                                 ),
-                              ),
-                              placeholder: (context, url) =>
-                                  const CircularProgressIndicator(),
-                              errorWidget: (context, url, error) =>
-                                  const Icon(Icons.error),
-                            )
-                          : const SizedBox(),
+                                placeholder: (context, url) =>
+                                    const CircularProgressIndicator(),
+                                errorWidget: (context, url, error) =>
+                                    const Icon(Icons.error),
+                              )
+                            : const SizedBox(),
+                      ),
                     ),
-                  ),
-                  const SizedBox(
-                    height: 20.0,
-                  ),
-                  Center(
-                      child: Text(
-                    crypto.name!.toString() +
-                        " (" +
-                        crypto.symbol!.toUpperCase() +
-                        ")",
-                    style: GoogleFonts.aBeeZee(
-                        fontWeight: FontWeight.bold, fontSize: 20.0),
-                  )),
-                  const SizedBox(
-                    height: 10.0,
-                  ),
-                  RowWidget(
-                    txtl: "Current Price",
-                    txtr: "\$ " + crypto.currentPrice.toString(),
-                  ),
-                  RowWidget(
-                    txtl: "High 24 hr",
-                    txtr: "\$ " + crypto.high24h.toString(),
-                  ),
-                  RowWidget(
-                    txtl: "Low 24 hr",
-                    txtr: "\$ " + crypto.low24h.toString(),
-                  ),
-                  RowWidget(
-                    txtl: "Price Change 24 hr",
-                    txtr: "\$ " + crypto.priceChange24h!.toStringAsFixed(3),
-                  ),
-                  RowWidget(
-                    txtl: "Price Change % 24 hr",
-                    txtr: crypto.priceChangePercentage24h.toString() + " %",
-                  ),
-                  RowWidget(
-                    txtl: "Current Price",
-                    txtr: "\$ " + crypto.currentPrice.toString(),
-                  ),
-                  RowWidget(
-                    txtl: "Market Cap",
-                    txtr: "\$ " + crypto.marketCap.toString(),
-                  ),
-                  RowWidget(
-                    txtl: "Market Cap Change % 24 hr",
-                    txtr: crypto.marketCapChangePercentage24h.toString() + " %",
-                  ),
-                ],
+                    const SizedBox(
+                      height: 20.0,
+                    ),
+                    Center(
+                        child: Text(
+                      crypto.name!.toString() +
+                          " (" +
+                          crypto.symbol!.toUpperCase() +
+                          ")",
+                      style: GoogleFonts.aBeeZee(
+                          fontWeight: FontWeight.bold, fontSize: 20.0),
+                    )),
+                    const SizedBox(
+                      height: 10.0,
+                    ),
+                    RowWidget(
+                      txtl: "Current Price",
+                      txtr: "\$ " + crypto.currentPrice.toString(),
+                    ),
+                    RowWidget(
+                      txtl: "High 24 hr",
+                      txtr: "\$ " + crypto.high24h.toString(),
+                    ),
+                    RowWidget(
+                      txtl: "Low 24 hr",
+                      txtr: "\$ " + crypto.low24h.toString(),
+                    ),
+                    RowWidget(
+                      txtl: "Price Change 24 hr",
+                      txtr: "\$ " + crypto.priceChange24h!.toStringAsFixed(3),
+                    ),
+                    RowWidget(
+                      txtl: "Price Change % 24 hr",
+                      txtr: crypto.priceChangePercentage24h.toString() + " %",
+                    ),
+                    RowWidget(
+                      txtl: "Current Price",
+                      txtr: "\$ " + crypto.currentPrice.toString(),
+                    ),
+                    RowWidget(
+                      txtl: "Market Cap",
+                      txtr: "\$ " + crypto.marketCap.toString(),
+                    ),
+                    RowWidget(
+                      txtl: "Market Cap Change % 24 hr",
+                      txtr:
+                          crypto.marketCapChangePercentage24h.toString() + " %",
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
--- lib/Screens/LandingPage.dart
+++ lib/Screens/LandingPage.dart
@@ -11,49 +11,65 @@ class LandingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.indigo[300],
-        appBar: AppBar(
-          title: Text(
-            "Krypto",
-            style: GoogleFonts.aBeeZee(
-                fontWeight: FontWeight.bold, letterSpacing: 3.0),
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: NetworkImage(
+            "https://images.unsplash.com/photo-1642104704074-907c0698cbd9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80",
           ),
-          foregroundColor: Colors.black,
-          backgroundColor: Colors.transparent,
-          elevation: 0.0,
+          fit: BoxFit.cover,
         ),
-        body: BlocBuilder<CryptoDataBloc, CryptoDataState>(
-          builder: (context, state) {
-            if (state is CryptoDataInitialState) {
-              context.read<CryptoDataBloc>().add(LoadCryptoDataEvent());
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            } else if (state is CryptoDataLoadingState) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            } else if (state is CryptoDataErrorState) {
-              return const Center(
-                child: Text("Oops! Something went wrong!"),
-              );
-            } else if (state is CryptoDataLoadedState) {
-              List<Crypto> cryptoList = state.cryptoList;
-              return RefreshIndicator(
-                onRefresh: () {
-                  context.read<CryptoDataBloc>().add(LoadCryptoDataEvent());
-                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                    content: Text('Refreshing'),
-                    behavior: SnackBarBehavior.floating,
-                  ));
-                  return Future.delayed(const Duration(microseconds: 1), () {});
-                },
-                child: BuildCryptoListWidget(cryptoList: cryptoList),
-              );
-            }
-            return Container();
-          },
-        ));
+      ),
+      child: Scaffold(
+          // extendBodyBehindAppBar: true,
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            title: Text(
+              "Krypto",
+              style: GoogleFonts.aBeeZee(
+                  fontWeight: FontWeight.bold, letterSpacing: 3.0),
+            ),
+            foregroundColor: Colors.black,
+            backgroundColor: Colors.transparent,
+            elevation: 0.0,
+          ),
+          body: BlocBuilder<CryptoDataBloc, CryptoDataState>(
+            builder: (context, state) {
+              if (state is CryptoDataInitialState) {
+                context.read<CryptoDataBloc>().add(LoadCryptoDataEvent());
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else if (state is CryptoDataLoadingState) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else if (state is CryptoDataErrorState) {
+                return const Center(
+                  child: Text("Oops! Something went wrong!"),
+                );
+              } else if (state is CryptoDataLoadedState) {
+                List<Crypto> cryptoList = state.cryptoList;
+                return RefreshIndicator(
+                  onRefresh: () {
+                    context.read<CryptoDataBloc>().add(LoadCryptoDataEvent());
+                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                      content: Text('Refreshing'),
+                      behavior: SnackBarBehavior.floating,
+                    ));
+                    return Future.delayed(
+                        const Duration(microseconds: 1), () {});
+                  },
+                  child: BuildCryptoListWidget(cryptoList: cryptoList),
+                );
+              }
+              return Container();
+            },
+          )),
+    );
   }
 }
+
+/* */
